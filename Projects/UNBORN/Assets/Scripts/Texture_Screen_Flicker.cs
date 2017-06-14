caedc85ef95859bf05fd2da68b379510c1e1d10f
Milan Wijnmaalen <info@milanwijnmaalen.com>
2017-06-14 21:33:05 +0200

    VO Avaline Scene 1-4 in Fmod

--- Projects/UNBORN/Assets/Scripts/Texture_Screen_Flicker.cs
+++ Projects/UNBORN/Assets/Scripts/Texture_Screen_Flicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class Texture_Screen_Flicker : MonoBehaviour {
 
@@ -9,10 +10,18 @@ public class Texture_Screen_Flicker : MonoBehaviour {
     public int FlickerSolved;
     public int DelayMin;
     public int DelayMax;
+    public bool mainScreen = false;
 
     // Use this for initialization
     void Start () {
         Mat = gameObject.GetComponent<Renderer>().material;
+
+        if (mainScreen)
+        {
+            
+            StudioEventEmitter mainscreen = GameObject.FindGameObjectsWithTag("Mainscreen_sound")[0].GetComponent<StudioEventEmitter>();
+            mainscreen.Play();
+        }
         StartCoroutine(ScreenFlickerDelay());
     }
 
@@ -26,6 +35,15 @@ public class Texture_Screen_Flicker : MonoBehaviour {
        yield return new WaitForSeconds(Random.Range(DelayMin,DelayMax));
         StartCoroutine(ScreenFlicker());
         LightOn = true;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if(transform.GetChild(i).transform.name == "screensound")
+            {
+                transform.GetChild(i).GetComponent<StudioEventEmitter>().Play();
+            }
+        }
+
         if (FlickerSolved == 1)
         {
             Mat.SetColor("_EmissionColor", (GetComponent<Renderer>().material.color * 2.5f));
@@ -40,6 +58,16 @@ public class Texture_Screen_Flicker : MonoBehaviour {
             Mat.SetColor("_EmissionColor", (GetComponent<Renderer>().material.color * 0));
             yield return new WaitForSeconds(Random.Range(0.3f,0.5f));
             Mat.SetColor("_EmissionColor", (GetComponent<Renderer>().material.color * 2.5f));
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (transform.GetChild(i).transform.name == "screensound")
+                {
+                    transform.GetChild(i).GetComponent<StudioEventEmitter>().Play();
+                }
+            }
+
+
             yield return new WaitForSeconds(Random.Range(0.3f, 0.5f));
         }
 
