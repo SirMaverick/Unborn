1c923e2639abcac5692b3b1b7c38b6159182483d
Milan Wijnmaalen <info@milanwijnmaalen.com>
2017-06-27 16:45:02 +0200

    Room 7 Music

--- Projects/UNBORN/Assets/Scripts/Scene2/HospitalDoorContainer.cs
+++ Projects/UNBORN/Assets/Scripts/Scene2/HospitalDoorContainer.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class HospitalDoorContainer : MonoBehaviour {
 
     public bool DoorMoving;
     public bool DoorOpen;
     public Transform newHospitalPos;
+
+	private bool previousmoving = false;
+
+	private void Update()
+	{
+		if(previousmoving == false && DoorMoving == true)
+		{
+			//TO DO (-Milan)
+			//RuntimeManager.PlayOneShot("event:/Room 02/SFX/Door_open", transform.position + new Vector3(0, 0, 0.85f));
+			
+		}
+		previousmoving = DoorMoving;
+	}
 
 }
