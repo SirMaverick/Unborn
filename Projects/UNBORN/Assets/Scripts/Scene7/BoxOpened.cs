580eb122bba21b5fd15a92ece8860ed4c0455193
SirMaverick <rickruiten@hotmail.com>
2017-07-04 14:19:08 +0200

    Minor fixes

---
+++ Projects/UNBORN/Assets/Scripts/Scene7/BoxOpened.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxOpened : MonoBehaviour {
+
+    GameObject lever;
+
+	// Use this for initialization
+	void Start () {
+        lever = GameObject.Find("Lever");
+        lever.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ReplaceSubtitles.instance.currentNode.character == "Sam" && ReplaceSubtitles.instance.currentNode.fmodVoiceOver == "Zin08c") {
+            lever.SetActive(true);
+            Destroy(this);            
+        }
+	}
+}
