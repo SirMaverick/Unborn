fec90433ae7084205c02c6e77178cd840dfe99be
SirMaverick <rickruiten@hotmail.com>
2017-07-04 16:24:25 +0200

    Added movies End scene

---
+++ Projects/UNBORN/Assets/Movies/RunVideo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RunVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+
+    RawImage image;
+    AudioSource AS;
+
+
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<RawImage>();
+        AS = GetComponent<AudioSource>();
+
+        PlayClip();
+		
+	}
+
+    void PlayClip() {
+        image.texture = movie;
+        movie.Play();
+        AS.clip = movie.audioClip;
+        AS.Play();
+    }
+
+}
